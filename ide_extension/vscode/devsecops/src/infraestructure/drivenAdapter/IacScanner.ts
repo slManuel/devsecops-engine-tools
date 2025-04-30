26a8fae4e8a0272a891acd2e38e589ed88f7fa70
luarredo <luarredo@bancolombia.com.co>
2025-04-30 08:55:40 -0500

    fix: Deleted console log

--- ide_extension/vscode/devsecops/src/infraestructure/drivenAdapter/IacScanner.ts
+++ ide_extension/vscode/devsecops/src/infraestructure/drivenAdapter/IacScanner.ts
@@ -6,7 +6,7 @@ import OutputManager from "../helper/OutputManager";
 export class IacScanner implements IScannerGateway{
 
     scan(elementToScan: string, outputChannel: OutputChannel, toolVersion: string): void {
-        exec(`/usr/local/bin/docker run --rm -v ${elementToScan}:/ms_artifact bancolombia/devsecops-engine-tools:${toolVersion}  devsecops-engine-tools --platform_devops local --remote_config_repo docker_default_remote_config --module engine_iac --tool checkov --folder_path /ms_artifact`, (error, stdout, stderr) => {
+        exec(`/usr/local/bin/docker run --rm -v ${elementToScan}:/ms_artifact artifactory.apps.bancolombia.com/devops/devsecops-engine-tools:${toolVersion}  devsecops-engine-tools --platform_devops local --remote_config_repo docker_default_remote_config --module engine_iac --tool checkov --folder_path /ms_artifact`, (error, stdout, stderr) => {
             if (error) {
                 console.error(`exec error: ${error}`);
                 console.error(`stderr: ${stderr}`);
