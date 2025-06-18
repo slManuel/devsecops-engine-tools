dbd772a8178c3aa4811035b0324debf896d29436
jcamilomolinar <jucmolin@bancolombia.com.co>
2025-06-18 15:52:15 -0500

    chore: delete jfrog test volume

--- ide_extension/vscode/devsecops/src/infraestructure/drivenAdapter/DependenciesScanner.ts
+++ ide_extension/vscode/devsecops/src/infraestructure/drivenAdapter/DependenciesScanner.ts
@@ -39,7 +39,7 @@ export class DependenciesScanner implements IScannerGateway {
         return;
       }
 
-      const dockerCommand = `${dockerPath} run --rm -v /home/jucmolin/jf:/root/jf -v ${elementToScan}:/ms_artifact ${dockerImageName}:${toolVersion} sh -c "devsecops-engine-tools --platform_devops local --remote_config_source local --xray_mode ${xrayMode} --remote_config_repo docker_default_remote_config --module engine_dependencies --tool ${dependenciesTool} --token_engine_dependencies ${dependenciesToken} --folder_path /ms_artifact --context true"`;
+      const dockerCommand = `${dockerPath} run --rm -v ${elementToScan}:/ms_artifact ${dockerImageName}:${toolVersion} sh -c "devsecops-engine-tools --platform_devops local --remote_config_source local --xray_mode ${xrayMode} --remote_config_repo docker_default_remote_config --module engine_dependencies --tool ${dependenciesTool} --token_engine_dependencies ${dependenciesToken} --folder_path /ms_artifact --context true"`;
 
       const childProcess = exec(dockerCommand, (error, stdout, stderr) => {
         clearTimeout(timeout);
