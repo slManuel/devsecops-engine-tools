26fdf5a473180672f41c2cd9372a433861232429
jcamilomolinar <jucmolin@bancolombia.com.co>
2025-07-17 17:08:27 -0500

    fix: print banner in integrations entry point

--- tools/devsecops_engine_tools/engine_utilities/engine_integrations/src/infrastructure/entry_points/entry_point_integrations.py
+++ tools/devsecops_engine_tools/engine_utilities/engine_integrations/src/infrastructure/entry_points/entry_point_integrations.py
@@ -1,11 +1,11 @@
-def init_engine_integrations(args):
-    pass
-
 from devsecops_engine_tools.engine_utilities.engine_integrations.src.domain.usecases.handle_integrations import (
     Integrations
 )
 from devsecops_engine_tools.engine_utilities.utils.logger_info import MyLogger
 from devsecops_engine_tools.engine_utilities import settings
+from devsecops_engine_tools.engine_utilities.utils.printers import (
+    Printers,
+)
 
 logger = MyLogger.__call__(**settings.SETTING_LOGGER).get_logger()
 
@@ -18,6 +18,12 @@ def init_engine_integrations(
     metrics_manager_gateway,
     args,
 ):
+    config_tool = remote_config_source_gateway.get_remote_config(
+        args["remote_config_repo"], "/engine_core/ConfigTool.json", args["remote_config_branch"]
+    )
+
+    Printers.print_logo_tool(config_tool["BANNER"])
+
     return Integrations(
         vulnerability_management_gateway=vulnerability_management_gateway,
         secrets_manager_gateway=secrets_manager_gateway,
