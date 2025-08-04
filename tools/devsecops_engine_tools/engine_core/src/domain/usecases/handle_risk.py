18a6b3e57dc70b7ecf3731b1e42427147180d2f5
Michael David Herrera Vargas <micherre@bancolombia.com.co>
2025-08-04 15:32:39 -0500

    feat(engine_risk): set to print PRINT_DOMAIN if it has a value

--- tools/devsecops_engine_tools/engine_core/src/domain/usecases/handle_risk.py
+++ tools/devsecops_engine_tools/engine_core/src/domain/usecases/handle_risk.py
@@ -208,11 +208,16 @@ class HandleRisk:
             dict_args, pipeline_name, service_list
         )
 
+        print_domain = remote_config["VULNERABILITY_MANAGER"]["DEFECT_DOJO"]["PRINT_DOMAIN"]
+        host_defect_dojo = remote_config["VULNERABILITY_MANAGER"]["DEFECT_DOJO"]["HOST_DEFECT_DOJO"]
         for engagement in new_service_list:
-            print(f"Service to analyze: {engagement.name}, URL: {engagement.vm_url}")
-            logger.info(
-                f"Service to analyze: {engagement.name}, URL: {engagement.vm_url}"
-            )
+            engagement_url = engagement.vm_url
+            if print_domain:
+                engagement_url = engagement.vm_url.replace(host_defect_dojo, print_domain)
+          
+            msg = f"Service to analyze: {engagement.name}, URL: {engagement_url}"
+            print(msg)
+            logger.info(msg)
 
         findings = []
         exclusions = []
