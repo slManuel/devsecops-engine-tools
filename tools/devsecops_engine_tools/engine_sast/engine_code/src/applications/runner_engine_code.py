916b55913b46a11171702d97e3b38fe59d726bbe
Manuel Solis <jmsolis@bancoagricola.com.sv>
2025-08-21 18:18:34 -0600

    feat(engine_code): added create instance flow to kiuwan analyzer

--- tools/devsecops_engine_tools/engine_sast/engine_code/src/applications/runner_engine_code.py
+++ tools/devsecops_engine_tools/engine_sast/engine_code/src/applications/runner_engine_code.py
@@ -7,13 +7,29 @@ from devsecops_engine_tools.engine_sast.engine_code.src.infrastructure.driven_ad
 from devsecops_engine_tools.engine_utilities.git_cli.infrastructure.git_run import (
     GitRun
 )
+from devsecops_engine_tools.engine_utilities.utils.logger_info import MyLogger
+from devsecops_engine_tools.engine_utilities import settings
+from devsecops_engine_tools.engine_sast.engine_code.src.infrastructure.driven_adapters.kiuwan.kiuwan_tool import get_kiuwan_instance
 
-def runner_engine_code(dict_args, tool, devops_platform_gateway, remote_config_source_gateway):
+
+logger = MyLogger.__call__(**settings.SETTING_LOGGER).get_logger()
+
+def runner_engine_code(dict_args, tool, devops_platform_gateway, remote_config_source_gateway, secret_tool):
     try:
+        logger.info("Selecting tool...")
         tool_gateway = None
         git_gateway = GitRun()
         if (tool == "BEARER"):
+            logger.info("Bearer tool selected...")
             tool_gateway = BearerTool()
+        elif (tool == "KIUWAN"):
+            logger.info("Kiuwan tool selected...")
+            tool_gateway = get_kiuwan_instance(
+                dict_args=dict_args,
+                devops_platform_gateway=devops_platform_gateway
+            )
+        
+        logger.info("Tool has been selected successfully")
 
         return init_engine_sast_code(
             devops_platform_gateway=devops_platform_gateway,
