d2e50f2196c8931330bcc88dc70fb633175cef56
Santiago Gaviria Oliveros <sagavir@bancolombia.com.co>
2025-08-27 19:03:49 -0500

    docs: update baseurl

--- docs/Docusaurus/docusaurus.config.js
+++ docs/Docusaurus/docusaurus.config.js
@@ -16,10 +16,10 @@ const config = {
   },
   themes: ["@docusaurus/theme-mermaid"],
   // Set the production url of your site here
-  url: 'https://bancolombia.github.io',
+  url: 'https://your-docusaurus-site.example.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/devsecops-engine-tools/docs/Docusaurus/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
