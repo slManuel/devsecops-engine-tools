760ca5b759c14f63ce4b0557585b58184e2bf519
Manuel Solis <jmsolis@bancoagricola.com.sv>
2025-09-05 15:36:11 -0600

    feat(engine_core): add custom variable to runtime_local

--- tools/devsecops_engine_tools/engine_core/src/infrastructure/driven_adapters/runtime_local/runtime_local.py
+++ tools/devsecops_engine_tools/engine_core/src/infrastructure/driven_adapters/runtime_local/runtime_local.py
@@ -76,5 +76,6 @@ class RuntimeLocal(DevopsPlatformGateway):
             "vm_product_type_name" : "DET_VM_PRODUCT_TYPE_NAME",
             "vm_product_name" : "DET_VM_PRODUCT_NAME",
             "vm_product_description" : "DET_VM_PRODUCT_DESCRIPTION",
+            "build_task":  "DET_APPLICATION_BUILD_TASK",
         }
         return os.environ.get(env_variables[variable], None)
