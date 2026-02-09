57e09a881cd86b78732c40fb8cd6b5fa984eea00
Jhoan Alvear <Jalvear@bancolombia.com>
2026-02-09 16:13:13 -0500

    feat(azuredevops): add AzurePipelineVariableAdapter for managing pipeline variables

---
+++ tools/devsecops_engine_tools/engine_utilities/azuredevops/infrastructure/__init__.py
@@ -0,0 +1,7 @@
+from devsecops_engine_tools.engine_utilities.azuredevops.infrastructure.azure_devops_api import AzureDevopsApi
+from devsecops_engine_tools.engine_utilities.azuredevops.infrastructure.azure_pipeline_variable_adapter import AzurePipelineVariableAdapter
+
+__all__ = [
+    "AzureDevopsApi",
+    "AzurePipelineVariableAdapter",
+]
