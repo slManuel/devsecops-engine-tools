fc52c1eed7bf1a3ba2588bf9fb91cb8fd8f46b72
Santiago Gaviria Oliveros <sagavir@bancolombia.com.co>
2026-04-21 12:20:32 -0500

    refactor: fix S5890: Type hints should be consistent

--- tools/devsecops_engine_tools/engine_utilities/defect_dojo/domain/models/product_type.py
+++ tools/devsecops_engine_tools/engine_utilities/defect_dojo/domain/models/product_type.py
@@ -1,5 +1,5 @@
 import dataclasses
-from typing import List
+from typing import List, Optional
 from devsecops_engine_tools.engine_utilities.utils.dataclass_classmethod import FromDictMixin
 
 
@@ -8,8 +8,8 @@ class ProductType(FromDictMixin):
     id: int = 0
     name: str = ""
     description: str = ""
-    critical_product: bool = None
-    key_product: bool = None
+    critical_product: Optional[bool] = None
+    key_product: Optional[bool] = None
     updated: str = ""
     created: str = ""
     members: List[int] = dataclasses.field(default_factory=list)
